8ef31e49df1743d268af4731851541586e1908f1
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-10-28 20:29:50 -0300

    feat: create service and consume api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
+
+
+import Header from './components/Header';
+
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento App', 'Backend'])
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    })
+  }, [])
 
 
-  function handleAddProject () {
-    setProjects([...projects, `Novo pojeto ${Date.now()}`])
+  async function handleAddProject () {
+    // setProjects([...projects, `Novo pojeto ${Date.now()}`])
+    const response = await api.post('/projects', {
+      title: 'Frontend com React',
+      owner: 'Marcio Rodrigues'
+    })
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
     <div className="App">
       <Header title='Homepage2' />
 
-      <img width={200} src={backgroundImage} alt='teste de img' />
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type='button' onClick={handleAddProject}>Add novo projeto</button>
